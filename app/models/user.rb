172b0e549388611e27ad0851a7bee35e1991ac8e
Yuki Sawabe <sawabe0v0@gmail.com>
2020-03-08 11:33:37 +0900

    add validation to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,11 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :article_likes, dependent: :destroy
   has_many :comment_likes, dependent: :destroy
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :account, presence: true, length: { maximum: 50 }
+  # 正規表現 「.」を２回以上繰り返さないように設定　例）foo@bar..com
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: { maximum: 255 },
+                    uniqueness: true,
+                    format: { with: VALID_EMAIL_REGEX }
 end
