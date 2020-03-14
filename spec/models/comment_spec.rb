e6a1003d47d6a1d791110bcf7e242214e79ae044
Yuki Sawabe <sawabe0v0@gmail.com>
2020-03-14 14:43:42 +0900

    add test to Comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,22 @@
 require "rails_helper"
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "正常系テスト" do
+    context "bodyが入力されている" do
+      let(:comment) {build(:comment)}
+      it "commentが作られる" do
+        expect(comment.valid?).to eq true
+      end
+    end
+  end
+
+  describe "異常系テスト" do
+    context "bodyが入力されていない" do
+      let(:comment) { build(:comment, body: nil)}
+      it "エラーする" do
+        comment.valid?
+        expect(comment.errors.messages[:body]).to include "can't be blank"
+      end
+    end
+  end
 end
