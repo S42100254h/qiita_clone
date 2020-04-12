fdeda50def5815144980f0a52f10c519991a0b33
Yuki Sawabe <sawabe0v0@gmail.com>
2020-04-12 10:43:17 +0900

    create test for "POST /api/v1/auth/sign_in"

--- spec/requests/api/v1/registrations_spec.rb
+++ spec/requests/api/v1/registrations_spec.rb
@@ -69,4 +69,42 @@ RSpec.describe "Api::V1::Auth::Registrations", type: :request do
       end
     end
   end
+
+  describe "POST /api/v1/auth/sign_in" do
+    subject { post(api_v1_user_session_path, params: params) }
+
+    context "正しい情報が送られたとき" do
+      let!(:user) { create(:user) }
+      let(:params) { { email: user.email, password: user.password } }
+
+      it "ログインできる" do
+        subject
+        headers = response.headers
+        expect(headers["uid"]).to be_present
+        expect(headers["client"]).to be_present
+        expect(headers["access-token"]).to be_present
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "emailが間違っていたとき" do
+      let!(:user) { create(:user) }
+      let(:params) { { email: Faker::Internet.email, password: user.password } }
+
+      it "ログインできない" do
+        subject
+        expect(response).to have_http_status(401)
+      end
+    end
+
+    context "passwordが間違っていたとき" do
+      let!(:user) { create(:user) }
+      let(:params) { { email: user.email, password: Faker::Internet.password(min_length: 6, max_length: 128) } }
+
+      it "ログインできない" do
+        subject
+        expect(response).to have_http_status(401)
+      end
+    end
+  end
 end
