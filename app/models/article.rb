8161bfd88525364d5c7d069efae23640e8810435
Yuki Sawabe <sawabe0v0@gmail.com>
2020-04-20 22:24:41 +0900

    setup enum

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,8 @@ class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :article_likes, dependent: :destroy
   belongs_to :user
+  enum status: { draft: 0, published: 1, deleted: 2 }
   validates :title, presence: true, length: { maximum: 50 }
   validates :body, presence: true
+  validates :status, inclusion: { in: Article.statuses.keys }
 end
