40883afcc462ee8cd200fb5e1bbc7146476b6722
Yuki Sawabe <sawabe0v0@gmail.com>
2020-04-26 15:23:31 +0900

    modify current/articles_spec to use trait

--- spec/requests/api/v1/current/articles_spec.rb
+++ spec/requests/api/v1/current/articles_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe "Api::V1::Current::Articles", type: :request do
     subject { get(api_v1_current_articles_path, headers: headers) }
 
     before do
-      create_list(:article, 3, user: current_user, status: "draft")
-      create_list(:article, 5, user: current_user, status: "published")
-      create_list(:article, 7, user: other_user, status: "draft")
-      create_list(:article, 9, user: other_user, status: "published")
+      create_list(:article, 1, :draft, user: current_user)
+      create_list(:article, 2, :published, user: current_user)
+      create_list(:article, 4, :draft, user: other_user)
+      create_list(:article, 8, :published, user: other_user)
     end
 
     let(:current_user) { create(:user) }
@@ -18,7 +18,7 @@ RSpec.describe "Api::V1::Current::Articles", type: :request do
     it "記事一覧（ステータスが公開）を取得できる" do
       subject
       res = JSON.parse(response.body)
-      expect(res.length).to eq 5
+      expect(res.length).to eq 2
       expect(res[0].keys).to eq ["id", "title", "body", "updated_at", "status", "user"]
       expect(response).to have_http_status(:ok)
     end
