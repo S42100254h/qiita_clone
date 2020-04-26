5fb231ee40efbf767edd5fef6239fc1b141d0c91
Yuki Sawabe <sawabe0v0@gmail.com>
2020-04-26 10:05:16 +0900

    add test about show action

--- spec/requests/api/v1/articles/drafts_spec.rb
+++ spec/requests/api/v1/articles/drafts_spec.rb
@@ -20,4 +20,43 @@ RSpec.describe "Api::V1::Articles::Drafts", type: :request do
       expect(response).to have_http_status(:ok)
     end
   end
+
+  describe "GET /api/v1/articles/drafts:id" do
+    subject { get(api_v1_articles_draft_path(article.id), headers: headers) }
+    let(:current_user) { create(:user) }
+    let(:other_user) {create(:user) }
+
+    context "ログインユーザーが、自分の下書き記事詳細を取得する場合" do
+      let(:article) { create(:article, user: current_user, status: "draft") }
+      let(:headers) { current_user.create_new_auth_token }
+
+      it "取得できる" do
+        subject
+        res = JSON.parse(response.body)
+        expect(res["id"]).to eq article.id
+        expect(res["title"]).to eq article.title
+        expect(res["body"]).to eq article.body
+        expect(res["updated_at"]).to be_present
+        expect(res["status"]).to eq "draft"
+      end
+    end
+
+    context "ログインユーザーが、他のユーザーの下書き記事詳細を取得する場合" do
+      let(:article) { create(:article, user: other_user, status: "draft") }
+      let(:headers) { current_user.create_new_auth_token }
+
+      it "取得できない" do
+        expect { subject }.to raise_error ActiveRecord::RecordNotFound
+      end
+    end
+
+    context "他のユーザーが、ログインユーザーの下書き記事詳細を取得する場合" do
+      let(:article) { create(:article, user: current_user, status: "draft") }
+      let(:headers) { other_user.create_new_auth_token }
+
+      it "取得できない" do
+        expect { subject }.to raise_error ActiveRecord::RecordNotFound
+      end
+    end
+  end
 end
